faa3955503eaf4b682533516315c294675f4175c
ViniMortinho <vinimortinho@gmail.com>
2023-11-07 19:04:58 -0300

    Convenções de classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemplosFundamentos.models;
+
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Vini";
+pessoa1.Idade = 40;
+pessoa1.Apresentar();
