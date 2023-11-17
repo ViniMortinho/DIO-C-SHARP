eaa48cebfda91cb70d0531153ebbc6b8e54e4007
ViniMortinho <vinimortinho@gmail.com>
2023-11-17 00:56:41 -0300

    Conversão para String

--- Program.cs
+++ Program.cs
@@ -1,4 +1,73 @@
-﻿
+﻿//CONVERSÃO PARA STRING
+//CASOS DE USO:
+//1. Para concatenar strings;
+//2. Para realizar operações de busca e substituição em um texto (buscar palavra, substituir
+//palavra);
+//3. Para criptografia ou decifragem de dados;
+//4. Para armazenamento de informações no banco de dados;
+//5. Para enviar mensagens por email;
+//6. Para impressão de relatórios;
+//7. Para geração de códigos QR;
+//8. Para conversões de tipos de variáveis;
+//9. Para validação de entrada de dados;
+//10. Para formatar números;
+//11. Para gerar nomes de arquivos;
+//12. Para gerar URLs;
+//13. Para obter a representação do valor numérico como string;
+//14. Para obter o tamanho da string;
+//15. Para obter o comprimento da string;
+//16. Para obter os caracteres especiais de uma string;
+//17. Para obter as vogais de uma string;
+//18. Para obter as consoantes de uma string;
+//19. Para obter as letras minúsculas de uma string;
+//20. Para obter as letras maiúsculas de uma string;
+//21. Para obter as primeiras letras de cada palavra de uma string;
+//22. Para obter as últimas letras de cada palavra de uma string;
+//23. Para obter as palavras com mais de 5 letras de uma string;
+//24. Para obter as palavras com menos de 5 letras de uma string;
+//25. Para obter as palavras que começam com letra específica;
+//26. Para obter as palavras que terminam com letra específica;
+//27. Para obter as palavras que contêm letra específica;
+//28. Para obter as palavras que não contêm letra específica;
+//29. Para obter as palavras que são divisíveis por outro número;
+//30. Para obter as palavras que não são divisíveis por outro número;
+//31. Para obter as palavras que são pares;
+//32. Para obter as palavras que são impares;
+//33. Para obter as palavras que tem o mesmo comprimento de outras palavras;
+//34. Para obter as palavras que tem o mesmo tamanho de outras palavras;
+//35. Para obter as palavras que estão na mesma posição de outras palavras;
+//36. Para obter as palavras que estão fora da mesma posição de outras palavras;
+//37. Para obter as palavras que estão antes das palavras de outros grupos;
+//38. Para obter as palavras que estão depois das palavras de outros grupos;
+//39. Para obter as palavras que estão entre as palavras de outros grupos;
+//40. Para obter as palavras que estão dentro dos limites de outro grupo;
+//41. Para obter as palavras que estão fora dos limites de outro grupo;
+//42. Para obter as palavras que estão acima de outras palavras;
+//43. Para obter as palavras que estão abaixo de outras palavras;
+//44. Para obter as palavras que estão à esquerda de outras palavras;
+//45. Para obter as palavras que estão à direita de outras palavras;
+//46. Para obter as palavras que estão centralizadas em relação a outra palavra;
+//47. Para obter as palavras que estão descentralizadas em relação a outra palavra;
+//48. Para obter as palavras que estão repetidas;
+//49. Para obter as palavras que estão sem repetições;
+//50. Para obter as palavras que estão em ordem alfabética crescente;
+
+
+using ExemplosFundamentos.models;
+int inteiro = 5;
+
+string a = inteiro.ToString ();
+
+Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
 
 
 
